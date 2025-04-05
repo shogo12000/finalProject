441ae151d4f266730b600d2ec1fac45de481d5fe
sidney <shogo12000@hotmail.com>
2025-04-05 02:03:16 -0700

    fix menu

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,16 +27,7 @@ export default function TabLayout() {
         }),
       }}
     >
-      {/* <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
-        }}
-      />
-  */}
+ 
       <Tabs.Screen
         name="index"
         options={{
